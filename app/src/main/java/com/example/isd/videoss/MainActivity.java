f13e9844c04417fdfedb97c2ae18dd1cde1d7dd6
SAJID LATIF <indus1530@gmail.com>
2020-02-26 19:00:10 +0500

    layoutchanges

--- app/src/main/java/com/example/isd/videoss/MainActivity.java
+++ app/src/main/java/com/example/isd/videoss/MainActivity.java
@@ -7,6 +7,9 @@ import androidx.databinding.DataBindingUtil;
 
 import com.example.isd.videoss.databinding.ActivityMainBinding;
 
+import java.text.SimpleDateFormat;
+import java.util.concurrent.TimeUnit;
+
 
 public class MainActivity extends Activity {
 
@@ -19,6 +22,41 @@ public class MainActivity extends Activity {
         bi.setCallback(this);
 
 
+    }
+
+
+    public static long getDateDiff(SimpleDateFormat format, String oldDate, String newDate) {
+        try {
+            return TimeUnit.DAYS.convert(format.parse(newDate).getTime() - format.parse(oldDate).getTime(), TimeUnit.MILLISECONDS);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    public void BtnCheck() {
+
+        bi.date1.getText().toString();
+        bi.date2.getText().toString();
+
+        bi.check.setText(getDateDiff(new SimpleDateFormat("dd/MM/yyyy"), bi.date1.getText().toString(), bi.date2.getText().toString()));
+
+
+        /*if (formValidation()) {
+            try {
+                SaveDraft();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            if (UpdateDB()) {
+//                if (bi.RS16a.isChecked()) {
+//                    finish();
+//                    startActivity(new Intent(this, EndingActivity.class));
+//                } else {
+//                    Toast.makeText(this, "Failed to Update Database!", Toast.LENGTH_SHORT).show();
+//                }
+            }
+        }*/
     }
 
 
