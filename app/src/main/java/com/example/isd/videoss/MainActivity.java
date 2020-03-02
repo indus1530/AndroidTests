3352d9903d17f661faef58d4e3b94e090d8052ca
SAJID LATIF <indus1530@gmail.com>
2020-03-02 13:31:19 +0500

    not working

--- app/src/main/java/com/example/isd/videoss/MainActivity.java
+++ app/src/main/java/com/example/isd/videoss/MainActivity.java
@@ -11,7 +11,9 @@ import androidx.databinding.DataBindingUtil;
 
 import com.example.isd.videoss.databinding.ActivityMainBinding;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 
@@ -19,12 +21,74 @@ public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding bi;
 
+    DateTimeUtils obj = new DateTimeUtils();
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/M/yyyy hh:mm:ss");
+
+    try
+    ParseException e catch(
+
+    {
+        Date date1 = null;
+        try {
+            date1 = simpleDateFormat.parse("10/10/2013 11:30:10");
+        } catch (ParseException ex) {
+            ex.printStackTrace();
+        }
+        Date date2 = null;
+        try {
+            date2 = simpleDateFormat.parse("13/10/2013 20:35:55");
+        } catch (ParseException ex) {
+            ex.printStackTrace();
+        }
+
+        obj.printDifference(date1, date2);
+
+    })
+
+    {
+        e.printStackTrace();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         bi = DataBindingUtil.setContentView(this, R.layout.activity_main);
         bi.setCallback(this);
 
+
+    }
+
+
+    //1 minute = 60 seconds
+//1 hour = 60 x 60 = 3600
+//1 day = 3600 x 24 = 86400
+    public void printDifference(Date startDate, Date endDate) {
+        //milliseconds
+        long different = endDate.getTime() - startDate.getTime();
+
+        System.out.println("startDate : " + startDate);
+        System.out.println("endDate : " + endDate);
+        System.out.println("different : " + different);
+
+        long secondsInMilli = 1000;
+        long minutesInMilli = secondsInMilli * 60;
+        long hoursInMilli = minutesInMilli * 60;
+        long daysInMilli = hoursInMilli * 24;
+
+        long elapsedDays = different / daysInMilli;
+        different = different % daysInMilli;
+
+        long elapsedHours = different / hoursInMilli;
+        different = different % hoursInMilli;
+
+        long elapsedMinutes = different / minutesInMilli;
+        different = different % minutesInMilli;
+
+        long elapsedSeconds = different / secondsInMilli;
+
+        System.out.printf(
+                "%d days, %d hours, %d minutes, %d seconds%n",
+                elapsedDays, elapsedHours, elapsedMinutes, elapsedSeconds);
     }
 
 
